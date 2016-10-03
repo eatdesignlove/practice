dcd907daba8e05e8da6ad962b3615c076e3f2a72
eatdesignlove <eatdesignlove@gmail.com>
2016-10-03 21:18:11 +0900

    Add [Gear-VR] gulp-imagemin을 활용한 이미지 최적화

--- gear-vr/gulpfile.js
+++ gear-vr/gulpfile.js
@@ -3,22 +3,32 @@
 // 의존 모듈 로드
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+
 // 기본 업무 등록
 gulp.task('default', [
   'compile:css', 
+  'optimize:img',
   'watch'
 ]);
-// 관찰업무 등록
+
+// Watch
 gulp.task('watch', function(){
   gulp.watch('./src/**/*.{sass,scss}', ['compile:css']);
 });
 
-// compile:css
+// Compile:css
 gulp.task('compile:css', function(){
   return gulp.src('./src/_sass/*.{sass, scss}')
              .pipe(sass().on('error', sass.logError))
-             .pipe(gulp.dest('./dist/'+ 'css'));
+             .pipe(gulp.dest('./dist/css'));
 });
 
+// Optimize:img
+gulp.task('optimize:img', function(){
+  return gulp.src('./src/assets/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/assets/img'))
+})
